5d72680f644377ee6fddb173553e65e42ef1a63c
Surya0265 <suryaprakashb265@gmail.com>
2025-01-20 22:50:45 +0530

    1st problem

---
+++ climbing Stairs.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int climbStairs(int n) {
+    int a = 1;
+    int b = 2;
+    int c;
+    if (n == 1 || n == 2) return n;
+
+    for (int i = 3; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return c;
+}
+
+int main() {
+    int n;
+    printf("Enter the number of stairs: ");
+    scanf("%d", &n);
+
+    int ways = climbStairs(n);
+    printf("Number of ways to climb %d stairs is: %d\n", n, ways);
+
+    return 0;
+}
