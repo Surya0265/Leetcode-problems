7310ecec793cef2206bf54bc15e3dc94811a36bf
Surya0265 <suryaprakashb265@gmail.com>
2025-02-01 21:00:02 +0530

    daily question 1

---
+++ Arrays/daily questions/Special Array I.c
@@ -0,0 +1,20 @@
+bool isArraySpecial(int* nums, int numsSize) {
+    
+    for(int i=0;i<numsSize-1;i++){
+        if(nums[i]%2==0&&nums[(i+1)]%2==0){
+            return false;
+        }
+        if(nums[i]%2!=0&&nums[(i+1)]%2!=0){
+            return false;
+        }
+               
+               
+
+
+    }
+    return true;
+
+
+
+    
+}
