98c59a9ebb67d9580022f8ea83dba52083e78bf2
Surya0265 <suryaprakashb265@gmail.com>
2025-03-07 07:36:08 +0530

    33

---
+++ 2D Marrix/Rotate Image.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        for i in range(0,len(matrix[0])):
+            for j in range(i,len(matrix)):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+        for i in range(0,len(matrix)):
+            left=0
+            right=len(matrix[0])-1
+            while(left<right):
+                matrix[i][left],matrix[i][right]=matrix[i][right],matrix[i][left]
+                left+=1
+                right-=1
+        
+         
+
+
+        
