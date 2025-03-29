528b4f50ca2d6ae722990f7b67660611dce30c1f
Surya0265 <suryaprakashb265@gmail.com>
2025-03-29 19:42:11 +0530

    40

---
+++ Math/Find Three Consecutive Integers That Sum to a Given Number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def sumOfThree(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        if(num%3==0):
+            x=num/3
+            return [x-1,x,x+1]
+        
+        return []
+
+
+
+
+        
