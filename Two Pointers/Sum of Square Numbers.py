0fdfec3efe3cc249ae7e40a57ca2de8e4116611d
Surya0265 <suryaprakashb265@gmail.com>
2025-04-22 20:57:39 +0530

    58

---
+++ Two Pointers/Sum of Square Numbers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        l=0
+        r=int(math.isqrt(c))
+    
+        while(l<=r):
+              if (l*l)+(r*r)==c:
+                return True
+              elif(l*l)+(r*r)>c:
+                  r-=1
+              else:
+                l+=1
+        return False
+        
