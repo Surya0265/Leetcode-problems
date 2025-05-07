06f96c2a976a48478633dfbce727ac5ab2a369ea
Surya0265 <suryaprakashb265@gmail.com>
2025-05-08 03:12:33 +0530

    63

--- Backtracking/N-Queens.py
+++ Backtracking/N-Queens.py
@@ -38,3 +38,43 @@ def board_util(board,row,n):
 n=int(input("Enter size"))
 board=[['.'for i in range(n)]for j in range(n)]
 board_util(board,0,n)
+
+# leetcode solutions
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        def board_util(board, row, n):
+            if row == n:
+                res.append(["".join(r)for r in board])
+                return 
+            else:
+
+                for col in range(n):
+                    if issafe(board, row, col, n):
+                        board[row][col] = "Q"
+                        board_util(board, row + 1, n)
+                        board[row][col] = "."
+
+        def issafe(board, row, col, n):
+            for i in range(row):
+                if board[i][col] == "Q":
+                    return False
+            i, j = row, col
+            while i >= 0 and j >= 0:  # upper left diagonal
+                if board[i][j] == "Q":
+                    return False
+                i -= 1
+                j -= 1
+
+            i, j = row, col
+            while i >= 0 and j < n:  # upper right diagonal
+                if board[i][j] == "Q":
+                    return False
+                i -= 1
+                j += 1
+            return True
+
+        board = [["." for i in range(n)] for j in range(n)]
+        res=[]
+        board_util(board, 0, n)
+        return res
+      
