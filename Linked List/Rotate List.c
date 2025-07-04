0246b3680624f57b3ef69c9c3345aef7aae84016
Surya0265 <suryaprakashb265@gmail.com>
2025-07-04 07:49:06 +0530

    .

--- Linked List/Rotate List.c
+++ Linked List/Rotate List.c
@@ -31,3 +31,54 @@ struct ListNode* rotateRight(struct ListNode* head, int k) {
 
     return newhead;
 }
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# Function to build linked list from a list of numbers
+def build_linked_list(nums):
+    if not nums:
+        return None
+    head = ListNode(nums[0])
+    current = head
+    for num in nums[1:]:
+        current.next = ListNode(num)
+        current = current.next
+    return head
+
+# Function to reverse the linked list
+def reverseList(head):
+    prev = None
+    current = head
+    while current:
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+    return prev
+
+# Function to print linked list
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.val, end=" ")
+        current = current.next
+    print()
+
+# -------------------------------
+# Get input from user
+nums = list(map(int, input("Enter list elements separated by space: ").split()))
+
+# Build linked list
+head = build_linked_list(nums)
+
+print("Original list:")
+print_linked_list(head)
+
+# Reverse linked list
+reversed_head = reverseList(head)
+
+print("Reversed list:")
+print_linked_list(reversed_head)
