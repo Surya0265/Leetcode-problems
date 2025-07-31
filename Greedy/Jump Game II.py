5eff3390b9181e02943e3ab9f60384a946f55834
Surya0265 <suryaprakashb265@gmail.com>
2025-08-01 00:19:53 +0530

    161

---
+++ Greedy/Jump Game II.py
@@ -0,0 +1,11 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        current_end=0
+        farthest=0
+        jumps=0
+        for i in range(len(nums)-1):
+            farthest=max(farthest,i+nums[i])
+            if i==current_end:
+                jumps+=1
+                current_end=farthest
+        return jumps
