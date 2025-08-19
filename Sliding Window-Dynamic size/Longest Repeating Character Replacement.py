4ba953b21f0c8676e77f5e8c7ad55358aad2d707
Surya0265 <suryaprakashb265@gmail.com>
2025-08-19 22:02:32 +0530

    .

--- Sliding Window-Dynamic size/Longest Repeating Character Replacement.py
+++ Sliding Window-Dynamic size/Longest Repeating Character Replacement.py
@@ -1,23 +1,19 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-       count=[0]*26
-       max_freq=0
-       i=0
-       res=0
-       for j in range(len(s)):
-        count[ord(s[j])-ord('A')]+=1
-        max_freq=max(max_freq, count[ord(s[j])-ord('A')])
-        while(j-i+1)-max_freq>k:
-            count[ord(s[i])-ord('A')]-=1
-            i+=1
-        res=max(res,j-i+1)
-       return res
-
-
-
-
-
-                
-                
-
-        
+        i=0
+        m={}
+        maximum=0
+        j=0
+        while j<len(s):
+             m[s[j]]=m.get(s[j],0)+1
+             maxi=max(m.values())
+            
+             if (j-i+1)-maxi<=k:
+                maximum=max(maximum,j-i+1)
+             else:
+                if s[i] in m:
+                  m[s[i]]=m.get(s[i],0)-1
+                i+=1
+             j+=1
+       
+        return maximum
